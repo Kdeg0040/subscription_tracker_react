43edbc55f85ba43b541b619ce802f91730964699
karlo <deguzmankarlo@gmail.com>
2020-04-15 14:09:28 +0100

    App renders a Layout component

--- src/App.test.js
+++ src/App.test.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { shallow } from 'enzyme'
 
 import App from './App';
+import Layout from './components/Layout';
 
 const app = shallow(<App />);
 
 describe('<App />', () => {
   it('renders correctly', () => {
     expect(app).toMatchSnapshot();
+  });
+
+  it('renders a Layout component', () => {
+    expect(app.find(Layout).exists()).toBe(true);
   });
 });
