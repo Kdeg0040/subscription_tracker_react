3d0f872595ddcf013efed96b1cc9b6c70d404888
karlo <deguzmankarlo@gmail.com>
2020-04-22 12:47:23 +0100

    Restyle subscription display box

--- src/components/Subscription.js
+++ src/components/Subscription.js
@@ -4,8 +4,23 @@ class Subscription extends Component {
   render () {
     return (
     <div className="box has-background-grey-lighter">
-      {this.props.sub.id}. {this.props.sub.details.company}
-      <button className="btn-delete" onClick={ () => this.props.delete(this.props.sub.id) }>Delete</button>
+      <div className="media">
+
+        <div className="media-left">
+          <p className="image is-64x64">
+            <img src="https://bulma.io/images/placeholders/128x128.png" alt=""></img>
+          </p>
+        </div>
+
+        <div className="media-content">
+        <h1 className="title">{this.props.sub.details.company}</h1>
+        </div>
+
+        <div className="media-right">
+          <button className="btn-delete delete" onClick={ () => this.props.delete(this.props.sub.id) }>Delete</button>
+        </div>
+
+      </div>
     </div>
     );
   }
