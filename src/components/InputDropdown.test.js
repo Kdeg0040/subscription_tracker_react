3419512b824b7cbf7b4a7a36b9b3f0b3f725a49c
karlo <deguzmankarlo@gmail.com>
2020-04-26 14:46:50 +0100

    Company suggestions are rendered

--- src/components/InputDropdown.test.js
+++ src/components/InputDropdown.test.js
@@ -4,13 +4,14 @@ import { shallow } from 'enzyme';
 import InputDropdown from './InputDropdown';
 
 describe('<InputDropdown', () => {
-  const props = { 
+  const initialProps = { 
     formData: {
       company: '',
       suggestions: []
     } 
   };
-  const inputDropdown = shallow(<InputDropdown {...props} />);
+
+  const inputDropdown = shallow(<InputDropdown {...initialProps} />);
 
   it('renders correctly', () => {
     expect(inputDropdown).toMatchSnapshot();
@@ -18,5 +19,18 @@ describe('<InputDropdown', () => {
 
   it('initially does not render any dropdown items', () => {
     expect(inputDropdown.find('.dropdown-item').length).toEqual(0)
+  });
+
+  it('renders suggestions', () => {
+    const propsWithSuggestions = { 
+      formData: {
+        company: '',
+        suggestions: [{ name: 'test' }]
+      } 
+    }; 
+    const dropdownWithSuggestions = shallow(<InputDropdown {...propsWithSuggestions} />);
+ 
+    expect(dropdownWithSuggestions.find('.dropdown-item').length).toEqual(1)
+
   });
 });
