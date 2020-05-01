510a2f50df61d792577dc925c589fa6fffa59dde
karlo <deguzmankarlo@gmail.com>
2020-05-01 14:40:21 +0100

    Remove redundant section

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,8 @@
 import React from 'react';
 
 const layout = (props) => (
-  <div className="section">
-    <div className="column is-three-fifths is-offset-one-fifth has-text-centered">
-      {props.children}
-    </div>
+  <div className="column is-three-fifths is-offset-one-fifth has-text-centered">
+    {props.children}
   </div>
 );
 
