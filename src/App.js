387d1a109b2d6466de96122733480cca0e717013
karlo <deguzmankarlo@gmail.com>
2020-05-01 14:31:31 +0100

    Render Header component in Layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Layout from './components/Layout';
+import Header from './components/Header';
 import Subscriptions from './components/Subscriptions';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <Layout>
+      <Header />
       <Subscriptions />
       <Footer />
     </Layout>
