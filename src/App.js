9156649705db239678bc45ca32b9a614f8897212
karlo <deguzmankarlo@gmail.com>
2020-05-01 14:04:04 +0100

    Render Footer component in Layout

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Layout from './components/Layout';
 import Subscriptions from './components/Subscriptions';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Layout>
       <Subscriptions />
+      <Footer />
     </Layout>
   );
 }
