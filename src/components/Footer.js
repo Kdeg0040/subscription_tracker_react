dabcdb539d04beda7f4b4e75c96a4b0d429edbe7
karlo <deguzmankarlo@gmail.com>
2020-05-01 12:00:15 +0100

    Add clearbit attribution to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const footer = (props) => (
-  <div></div>
+  <div className="section">
+    <div className="footer">
+      <div className="content has-text-centered">
+        <p>
+          <a href="https://clearbit.com">Logos provided by Clearbit</a>
+        </p>
+      </div>
+    </div> 
+  </div>  
 )
 
 export default footer;
