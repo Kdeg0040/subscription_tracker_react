0afd26fa16d622a963cffca212eeb2810176ef0c
karlo <deguzmankarlo@gmail.com>
2020-05-01 14:09:57 +0100

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const header = (props) => (
+  <div></div>
+)
+
+export default header;
---
+++ src/components/Header.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Header from './Header';
+
+const header = shallow(<Header />);
+
+describe('<Header />', () => {
+  it('renders correctly', () => {
+    expect(header).toMatchSnapshot();
+  });
+});
