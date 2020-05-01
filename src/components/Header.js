9b62317f2f06c3b0d3a56df9e3d2cd92b0b4e293
karlo <deguzmankarlo@gmail.com>
2020-05-01 14:29:07 +0100

    Add hero banner to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const header = (props) => (
-  <div></div>
+  <div className="section is-full-width">
+    <div className="hero is-info is-bold">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">
+            Subscription Tracker
+          </h1>
+          <h2 className="subtitle">
+            Manage your recurring payments
+          </h2>
+        </div>
+      </div>
+    </div>
+  </div>
 )
 
 export default header;
