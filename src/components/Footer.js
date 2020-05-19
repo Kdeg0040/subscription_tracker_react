86b8920665471fca9a5ebf801f91e236721b8a42
karlo <deguzmankarlo@gmail.com>
2020-05-19 17:02:34 +0100

    Add React and Bulma links in Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const footer = (props) => (
     <div className="footer">
       <div className="content has-text-centered">
         <p>
-          <a href="https://clearbit.com">Logos provided by Clearbit</a>
+          Made with <a href="https://reactjs.org/">React</a> <i className="fab fa-react"></i> | Styled with <a href="https://bulma.io">Bulma</a> | Logos provided by <a href="https://clearbit.com">Clearbit</a>
         </p>
       </div>
     </div> 
